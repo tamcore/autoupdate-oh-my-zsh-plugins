cd9daceb93362e016e2e0c3ad84fa66575c63360
Paul Mabileau <paulmabileau@hotmail.fr>
2021-01-09 20:43:48 +0100

    Silence popd
    Redirect all output streams to /dev/null in order to suppress error
    messages when the directory stack is empty.
    
    Signed-off-by: Paul Mabileau <paulmabileau@hotmail.fr>

--- autoupdate.plugin.zsh
+++ autoupdate.plugin.zsh
@@ -47,7 +47,8 @@ function upgrade_oh_my_zsh_custom() {
     else
       printf "${RED}%s${NORMAL}\n" "There was an error updating $p. Try again later?"
     fi
-    popd -q
+
+    popd &>/dev/null
   done
 }
 
